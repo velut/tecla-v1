071aa128bc7a74f799d1dd869a733131fb1e9d31
velut <velut@users.noreply.github.com>
2019-07-26 19:00:08 +0200

    Refactor app package

--- server/pkg/app/app.go
+++ server/pkg/app/app.go
@@ -1,50 +1,132 @@
 package app
 
 import (
+	"reflect"
+	"strings"
+
 	"github.com/velut/tecla/server/pkg/core"
 	"github.com/velut/tecla/server/pkg/gui"
 )
 
 // App represents the main application.
 type App struct {
+	options *Options
+}
+
+// Options represents the options available to configure the application.
+type Options struct {
 	ConfigValidator core.ConfigValidatorAPI
 	Organizer       core.OrganizerAPI
 	WindowWidth     int
 	WindowHeight    int
+	ProductionMode  bool
 }
 
-// NewDefaultApp creates a new App with default implementations.
-func NewDefaultApp() *App {
-	return &App{
+// DevOptions returns a set of options for a development application.
+func DevOptions() *Options {
+	o := DefaultOptions()
+	o.ProductionMode = false
+	return o
+}
+
+// DefaultOptions returns a set of default options for a production application.
+func DefaultOptions() *Options {
+	return &Options{
 		ConfigValidator: core.NewConfigValidator(),
 		Organizer:       core.NewOrganizer(),
 		WindowWidth:     1280,
 		WindowHeight:    720,
+		ProductionMode:  true,
 	}
 }
 
-// Run runs the application and blocks.
+// NewApp returns a new application.
+// Options are required.
+func NewApp(options *Options) *App {
+	return &App{
+		options: options,
+	}
+}
+
+// Run starts the application and blocks until the GUI is closed.
 func (a *App) Run() error {
+	defer a.close()
 	return a.run()
 }
 
 func (a *App) run() error {
-	apiImpl := struct {
-		core.ConfigValidatorAPI
-		core.OrganizerAPI
-	}{
-		a.ConfigValidator,
-		a.Organizer,
-	}
-	return gui.Open(apiImpl, a.WindowWidth, a.WindowHeight)
+	return a.startGUI()
 }
 
-// Close closes the application.
-func (a *App) Close() error {
-	return a.close()
+func (a *App) startGUI() error {
+	gui := gui.NewGUI(a.guiOptions())
+	return gui.Start()
+}
+
+func (a *App) guiOptions() *gui.Options {
+	return &gui.Options{
+		Width:            a.options.WindowWidth,
+		Height:           a.options.WindowHeight,
+		BoundFuncs:       a.guiBoundFuncs(),
+		ProductionClient: a.options.ProductionMode,
+	}
+}
+
+func (a *App) guiBoundFuncs() []*gui.BoundFunc {
+	var bfs []*gui.BoundFunc
+
+	bfs = append(bfs, a.configValidatorMethods()...)
+	bfs = append(bfs, a.organizerMethods()...)
+
+	return bfs
+}
+
+func (a *App) configValidatorMethods() []*gui.BoundFunc {
+	api := reflect.TypeOf((*core.ConfigValidatorAPI)(nil)).Elem()
+	return extractMethods(api, a.options.ConfigValidator)
+}
+
+func (a *App) organizerMethods() []*gui.BoundFunc {
+	api := reflect.TypeOf((*core.OrganizerAPI)(nil)).Elem()
+	return extractMethods(api, a.options.Organizer)
+}
+
+func extractMethods(api reflect.Type, impl interface{}) []*gui.BoundFunc {
+	methodNames := extractAPIMethodNames(api)
+
+	var bfs []*gui.BoundFunc
+	implValue := reflect.ValueOf(impl)
+	for _, name := range methodNames {
+		// Binding name starts with a lower case character.
+		bindingName := strings.ToLower(string(name[0])) + name[1:]
+		bindingFunc := implValue.MethodByName(name).Interface()
+		bf := &gui.BoundFunc{
+			Name: bindingName,
+			Func: bindingFunc,
+		}
+		bfs = append(bfs, bf)
+	}
+
+	return bfs
+}
+
+func extractAPIMethodNames(api reflect.Type) []string {
+	var names []string
+	for i := 0; i < api.NumMethod(); i++ {
+		names = append(names, api.Method(i).Name)
+	}
+	return names
 }
 
 func (a *App) close() error {
-	_, err := a.Organizer.DropConfig()
+	if err := a.closeOrganizer(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *App) closeOrganizer() error {
+	_, err := a.options.Organizer.DropConfig()
 	return err
 }
